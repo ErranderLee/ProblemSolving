7f4461ff842081d1044bb69a8cf91a0dcd81804f
HoyongLee <85232033+ErranderLee@users.noreply.github.com>
2021-09-13 21:06:02 +0900

    Add files via upload

---
+++ 가장 큰 수.py
@@ -0,0 +1,8 @@
+def solution(numbers)
+    answer = ''
+    numbers = list(map(str, numbers))
+    numbers.sort(reverse=True, key=lambda xx3)
+    for item in numbers
+        answer += item
+
+    return str(int(answer))
