08cb2c1aca0098f59ac72db14a9123e379e6506e
ErranderLee <dre12am14@gmail.com>
2022-01-29 03:52:24 +0900

    신규 아이디 추천

--- 프로그래머스/로또의 최고 순위와 최저 순위.py
+++ 프로그래머스/로또의 최고 순위와 최저 순위.py
@@ -10,7 +10,7 @@ def solution(lottos, win_nums):
             count_for_zero += 1
     best = 6 - (count + count_for_zero) + 1
     worst = 6 - count + 1
-    answer.extend([best, worst if worst != 7 else 6])
+    answer.extend([best if best != 7 else 6, worst if worst != 7 else 6])
     return answer
 
-print(solution([44, 1, 0, 0, 31, 25], [31, 10, 45, 1, 6, 19]))
+print(solution([3, 2, 10, 5, 6, 1], [31, 10, 45, 1, 6, 19]))
